275521ea945444854d9542815e2c04b40e00b40c
Tarik EN NAKDI <t.ennakdi@gmail.com>
2016-11-02 15:54:26 +0100

    Update book.model.js

--- book.model.js
+++ book.model.js
@@ -1,1 +1,19 @@
-test
+'use strict';
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var BookSchema = new Schema ({
+	title: String,
+	published: {
+		type: Date,
+		default: Date.now
+},
+keywords: Array,
+published: Boolean,
+autor:{
+	type:Schema.objected,
+}
+})
+
+module.exports = mongoose.model('Book', BookSchema);
